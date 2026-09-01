c9410affba2f1e701952712a405c35c93ed1ab70
Bhavya Ahuja <bhavyaahuja493@gmail.com>
2026-09-02 01:17:30 +0530

    Time: 25 ms (18.5%), Space: 81.4 MB (93.42%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       Arrays.sort(nums);
+       for(int i=0; i<nums.length-1; i++){
+       if(nums[i]==nums[i+1])// it reprsent numbers inside array nums
+       {
+        return true;
+       }
+    }
+        return false;
+}
+       }

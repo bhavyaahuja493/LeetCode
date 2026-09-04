97347f0550bd396df09e10c0c0017427d4b28078
Bhavya Ahuja <bhavyaahuja493@gmail.com>
2026-09-04 18:43:11 +0530

    Time: 1 ms (98.06%), Space: 43.3 MB (71.87%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -1,9 +1,9 @@
 class Solution {
     public int scoreOfString(String s) {
-    int count=0;
+    int score=0;
     for(int i=0;i<s.length()-1;i++)
     {
-        count+=Math.abs(s.charAt(i) - s.charAt(i + 1));
+        score+=Math.abs(s.charAt(i) - s.charAt(i + 1));
     }
-    return count;
+    return score;
     }}

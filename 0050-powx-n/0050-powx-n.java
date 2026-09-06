790b9014ec07bfab8632ac86a7409dc4d9e21662
Bhavya Ahuja <bhavyaahuja493@gmail.com>
2026-09-06 22:25:35 +0530

    Time: 0 ms (100%), Space: 47.6 MB (77.87%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double res = 1;
+        long power = n; //stack overflow prevent
+        if (power < 0) {
+            x = 1 / x;
+            power = -power; //power is used so that stack doesnot overflow
+        }
+        while (power > 0) {
+            if (power % 2 == 1) {
+          res = res * x;}
+            x = x * x;
+            power = power / 2;
+        }
+        return res;
+    }
+}

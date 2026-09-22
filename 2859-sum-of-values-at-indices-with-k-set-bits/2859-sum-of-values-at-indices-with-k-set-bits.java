862c94a9292b670ca99a23e3f20e8ba87f3bae91
Bhavya Ahuja <bhavyaahuja493@gmail.com>
2026-09-22 19:33:23 +0530

    Time: 1 ms (100%), Space: 46.3 MB (78.96%) - LeetHub

---
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+         int temp = i;
+        int count = 0;
+      while (temp > 0) {
+           temp = temp & (temp - 1);
+          count++;
+            }
+            if (count == k) {
+                sum += nums.get(i);
+            }
+        }
+        return sum;
+    }
+}
